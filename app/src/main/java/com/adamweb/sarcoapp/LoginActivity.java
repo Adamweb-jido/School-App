b6ce1439c3777fc17eaf9c38a0214d37aa1a6a84
REAL_ADAMWEB <realadamweb@gmail.com>
2023-10-26 00:50:40 +0100

    Sarco Pixel, A school App

--- app/src/main/java/com/adamweb/sarcoapp/LoginActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/LoginActivity.java
@@ -2,13 +2,25 @@ package com.adamweb.sarcoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
 
+    MaterialButton register = findViewById(R.id.registerBtn);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
