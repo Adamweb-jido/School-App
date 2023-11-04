c61f57de9a71a2a2520fdc21789e02653392f0c6
REAL_ADAMWEB <realadamweb@gmail.com>
2023-11-04 15:42:14 +0000

    Sarco Pixel, A school App

--- app/src/main/java/com/adamweb/sarcoapp/HomeActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/HomeActivity.java
@@ -2,13 +2,27 @@ package com.adamweb.sarcoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ImageView photoAlbum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_acivity);
+
+        photoAlbum = findViewById(R.id.albumIcon);
+        photoAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AlbumActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
