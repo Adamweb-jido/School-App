2003cc172871a80b354a8bdc4c2757de697063b3
REAL_ADAMWEB <realadamweb@gmail.com>
2023-11-05 22:42:15 +0000

    Sarco Pixel, A school App

--- app/src/main/java/com/adamweb/sarcoapp/AlbumActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/AlbumActivity.java
@@ -1,14 +1,33 @@
 package com.adamweb.sarcoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlbumActivity extends AppCompatActivity {
+
+    RecyclerView albumRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
+
+        albumRecyclerView = findViewById(R.id.albumRecycler);
+
+        //Creating the List of the album items
+
+        List<AlbumList> albumLists = new ArrayList<>();
+        albumLists.add(new AlbumList("Adamu Ibrahim Ya'u", "csc/che", "08160807055", R.drawable.user_profile_dp));
+        albumLists.add(new AlbumList("Adamu Ibrahim Ya'u", "csc/che", "08160807055", R.drawable.user_profile_dp));
+        albumLists.add(new AlbumList("Adamu Ibrahim Ya'u", "csc/che", "08160807055", R.drawable.user_profile_dp));
+
+        albumRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        albumRecyclerView.setAdapter(new AlbumAdapter(getApplicationContext(), albumLists));
     }
 }
