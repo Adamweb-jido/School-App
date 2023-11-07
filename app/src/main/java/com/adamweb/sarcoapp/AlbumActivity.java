07680ac7ed4bd8c3419ba49149394f1931cff1e2
REAL_ADAMWEB <realadamweb@gmail.com>
2023-11-07 20:42:59 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/AlbumActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/AlbumActivity.java
@@ -24,7 +24,12 @@ public class AlbumActivity extends AppCompatActivity {
 
         List<Item> items = new ArrayList<>();
 
-        items.add(new Item("Adamu ibrahim ya'u", "realadamweb@gmail.com","csc/che", "08160807055",  R.drawable.adam));
+        items.add(new Item("Adamu ibrahim ya'u", "realadamweb@gmail.com","csc/che", "08160807055",  R.drawable.adamweb));
+        items.add(new Item("Zahraddin Yusif", "zahraddinyusif@gmail.com","csc/che", "08064130342", R.drawable.zdeennn));
+        items.add(new Item("Khalifa muhammad", "khalifamuhammad@gmail.com","csc/che", "08131830383",  R.drawable.khalifa));
+        items.add(new Item("aliyu sunusi", "aliyuhaidar@gmail.com","csc/che", "09063333861",  R.drawable.aliyu));
+        items.add(new Item("salisu ibrahim adam", "Salisibrahim@gmail.com","csc/che", "08160569598",  R.drawable.salis));
+        items.add(new Item("Hashim Abdullahi", "hashimabdullahi@gmail.com","csc/che", "08134345554",  R.drawable.monita));
 
 
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
