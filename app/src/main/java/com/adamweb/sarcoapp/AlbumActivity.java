9b5752f8e78165e93c37544697011755217ecdb4
REAL_ADAMWEB <realadamweb@gmail.com>
2023-11-07 08:56:38 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/AlbumActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/AlbumActivity.java
@@ -21,6 +21,7 @@ public class AlbumActivity extends AppCompatActivity {
 
 
         RecyclerView recyclerView = findViewById(R.id.albumRecycler);
+        int numberOfColumns = 3;
 
         List<Item> items = new ArrayList<>();
 
@@ -28,8 +29,21 @@ public class AlbumActivity extends AppCompatActivity {
         items.add(new Item("Adamweb Jido", "realadamweb@gmail.com", R.drawable.official));
         items.add(new Item("Adamweb Jido", "realadamweb@gmail.com", R.drawable.official));
         items.add(new Item("Adamweb Jido", "realadamweb@gmail.com", R.drawable.official));
+        GridLayoutManager layoutManager = new GridLayoutManager(this, numberOfColumns);
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (position % 3 == 0) {
+                    // Every third item spans two columns
+                    return 2;
+                } else {
+                    // Other items span one column
+                    return 1;
+                }
+            }
+        });
 
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(new MyAdapter(getApplicationContext(), items ));
     }
 }
