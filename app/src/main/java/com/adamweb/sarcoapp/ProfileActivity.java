6db3e1ed800205dc5b0c9244f15f61466d7db8ea
REAL_ADAMWEB <realadamweb@gmail.com>
2023-11-15 20:20:33 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/ProfileActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/ProfileActivity.java
@@ -2,15 +2,27 @@ package com.adamweb.sarcoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
-
+   ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imageView  = findViewById(R.id.backArrow);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
