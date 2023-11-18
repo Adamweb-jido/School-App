cd807d27f6c18d508ba65fb278072a92acacd61d
REAL_ADAMWEB <realadamweb@gmail.com>
2023-11-18 20:44:18 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/HomeActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/HomeActivity.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class HomeActivity extends AppCompatActivity {
 
     ImageView photoAlbum, chats, profile;
-
+    RecyclerView leaderRecycler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
