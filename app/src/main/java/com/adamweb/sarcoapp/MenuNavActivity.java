85eb8b7a317738a7221717377394e245ec72abc0
REAL_ADAMWEB <realadamweb@gmail.com>
2023-11-29 14:09:22 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/MenuNavActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/MenuNavActivity.java
@@ -12,19 +12,15 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MenuNavActivity extends AppCompatActivity {
 
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
 
-    HomeActivity homeItems = new HomeActivity();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_nav);
 
-        drawerLayout = findViewById(R.id.menu_drawer_layout);
-        navigationView = findViewById(R.id.menu_nav_view_id);
-        homeItems.menu = findViewById(R.id.menuIcon);
 
         drawerLayout.addDrawerListener((DrawerLayout.DrawerListener) homeItems.menu);
     }
