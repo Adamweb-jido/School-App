04e6d2de64b96c24f708afe5d485bbb02ea23809
REAL_ADAMWEB <realadamweb@gmail.com>
2023-12-09 17:32:09 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/OnBoardingActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/OnBoardingActivity.java
@@ -1,10 +1,13 @@
 package com.adamweb.sarcoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class OnBoardingActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/adamweb/sarcoapp/SliderAdapter.java
+++ app/src/main/java/com/adamweb/sarcoapp/SliderAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -51,6 +53,20 @@ public class SliderAdapter extends PagerAdapter {
 
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View view = layoutInflater.inflate(R.layout.slides_layout, container, false);
-        return super.instantiateItem(container, position);
+        ImageView imageView = view.findViewById(R.id.img_slides);
+        TextView title = view.findViewById(R.id.heading_title);
+        TextView description = view.findViewById(R.id.header_description);
+
+        imageView.setImageResource(images[position]);
+        title.setText(titles[position]);
+        description.setText(descriptions[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout) object);
     }
 }
