229ad560c4362da51591b3606962eec0d63b43a5
REAL_ADAMWEB <realadamweb@gmail.com>
2023-12-09 17:07:40 +0000

    another commit

---
+++ app/src/main/java/com/adamweb/sarcoapp/SliderAdapter.java
@@ -0,0 +1,56 @@
+package com.adamweb.sarcoapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SliderAdapter extends PagerAdapter {
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    int images[] = {
+            R.drawable.album_onboard,
+            R.drawable.chat_onboard,
+            R.drawable.connet_onboard
+    };
+
+    int titles[] = {
+            R.string.first_heading,
+            R.string.second_heading,
+            R.string.third_heading
+    };
+
+    int descriptions[] = {
+            R.string.first_onboard_text,
+            R.string.second_onboard_text,
+            R.string.third_onboard_text
+    };
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view==(ConstraintLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slides_layout, container, false);
+        return super.instantiateItem(container, position);
+    }
+}
