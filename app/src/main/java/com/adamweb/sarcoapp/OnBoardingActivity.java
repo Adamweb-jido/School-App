1e08d1e395d8793619d87db7341add5849ddba86
REAL_ADAMWEB <realadamweb@gmail.com>
2023-12-10 08:52:33 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/OnBoardingActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/OnBoardingActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,6 +23,7 @@ public class OnBoardingActivity extends AppCompatActivity {
     SliderAdapter sliderAdapter;
     TextView [] dots;
     MaterialButton done_btn;
+    Button skipBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +33,29 @@ public class OnBoardingActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.slider);
         dotsLayout = findViewById(R.id.dots);
         done_btn = findViewById(R.id.next_btn);
+        skipBtn = findViewById(R.id.skip_btn);
 
         sliderAdapter = new SliderAdapter(this);
         viewPager.setAdapter(sliderAdapter);
 
         dotsFunction(0);
         viewPager.addOnPageChangeListener(onPageChangeListener);
+
+        done_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
+
+        skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
     }
 
     private void dotsFunction(int position){
