705acebda52bff057606608f3a809e6b3982b48d
REAL_ADAMWEB <realadamweb@gmail.com>
2023-12-13 10:12:08 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/MainActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/MainActivity.java
@@ -4,25 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    MaterialButton myButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myButton = findViewById(R.id.getStart);
-        myButton.setOnClickListener(new View.OnClickListener() {
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), OnBoardingActivity.class);
-                startActivity(intent);
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
-        });
+        })
     }
 }
