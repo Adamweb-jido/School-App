90f6f8456c8306dd662fb6dfc445f8273d10f2cd
REAL_ADAMWEB <realadamweb@gmail.com>
2023-12-16 07:47:43 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/MainActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/MainActivity.java
@@ -6,14 +6,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
