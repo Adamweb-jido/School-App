c1b63038e0b98635021e70fb499037f483eea9f3
REAL_ADAMWEB <realadamweb@gmail.com>
2023-12-27 11:04:19 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/UserReadWriteData.java
+++ app/src/main/java/com/adamweb/sarcoapp/UserReadWriteData.java
@@ -9,4 +9,36 @@ public class UserReadWriteData {
         this.admissionNumber = admissionNumber;
         this.phoneNumber = phoneNumber;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAdmissionNumber() {
+        return admissionNumber;
+    }
+
+    public void setAdmissionNumber(String admissionNumber) {
+        this.admissionNumber = admissionNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
