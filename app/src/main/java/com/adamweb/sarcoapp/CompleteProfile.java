8a9e5b4b3e09a41a73d5eb781b7002138e057df2
REAL_ADAMWEB <realadamweb@gmail.com>
2023-12-30 15:41:48 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/CompleteProfile.java
+++ app/src/main/java/com/adamweb/sarcoapp/CompleteProfile.java
@@ -10,13 +10,15 @@ import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 public class CompleteProfile extends AppCompatActivity {
 
     String [] combs = {"Computer/Biology", "Computer/Chemistry", "Computer/Physics", "Computer/Maths"};
-    ArrayAdapter<String> combsAdapter;
     MaterialAutoCompleteTextView materialAutoCompleteTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_profile);
 
-        materialAutoCompleteTextView = findViewById()
+        materialAutoCompleteTextView = findViewById(R.id.autoComplete);
+
+        materialAutoCompleteTextView.setAdapter(new ArrayAdapter<String>(this, R.layout.comb_layout, combs));
+
     }
 }
