96a762311dcf14e181d9a8585a7b327a865b27b5
REAL_ADAMWEB <realadamweb@gmail.com>
2023-12-31 21:30:12 +0000

    another commit

--- app/src/main/java/com/adamweb/sarcoapp/CompleteProfile.java
+++ app/src/main/java/com/adamweb/sarcoapp/CompleteProfile.java
@@ -1,24 +1,55 @@
 package com.adamweb.sarcoapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+
+import java.net.URI;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CompleteProfile extends AppCompatActivity {
 
     String [] combs = {"Computer/Biology", "Computer/Chemistry", "Computer/Physics", "Computer/Maths"};
     MaterialAutoCompleteTextView materialAutoCompleteTextView;
+    CircleImageView imageView;
+    FloatingActionButton floatingActionButton;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_profile);
 
         materialAutoCompleteTextView = findViewById(R.id.autoComplete);
+        imageView = findViewById(R.id.circleImageView);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
 
         materialAutoCompleteTextView.setAdapter(new ArrayAdapter<String>(this, R.layout.comb_layout, combs));
 
+
+        floatingActionButton.setOnClickListener(v ->
+                ImagePicker.with(CompleteProfile.this)
+                .crop()	    			//Crop image(Optional), Check Customization for more option
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start());
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        assert data != null;
+        Uri uri = data.getData();
+        imageView.setImageURI(uri);
     }
 }
