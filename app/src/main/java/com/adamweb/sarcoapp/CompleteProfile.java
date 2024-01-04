5756c33f11a17f0319136f623c192b977b34d93d
REAL_ADAMWEB <realadamweb@gmail.com>
2024-01-04 22:56:34 +0000

    Another Commit

--- app/src/main/java/com/adamweb/sarcoapp/CompleteProfile.java
+++ app/src/main/java/com/adamweb/sarcoapp/CompleteProfile.java
@@ -80,9 +80,7 @@ public class CompleteProfile extends AppCompatActivity {
             StorageReference imageFile = storageReference.child(firebaseAuth.getCurrentUser().getUid() + "." + getFileExtension(profileImage));
             imageFile.putFile(profileImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
-                }
             })
         }
     }
