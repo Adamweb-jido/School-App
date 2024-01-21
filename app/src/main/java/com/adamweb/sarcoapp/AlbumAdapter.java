27fbf6bca4bef18999471967d5123e1437be9515
REAL_ADAMWEB <realadamweb@gmail.com>
2024-01-21 13:49:07 +0100

    Another Commit

--- app/src/main/java/com/adamweb/sarcoapp/AlbumAdapter.java
+++ app/src/main/java/com/adamweb/sarcoapp/AlbumAdapter.java
@@ -6,12 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
