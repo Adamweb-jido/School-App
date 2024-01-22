c741c673f9c693df662d472dd7919d2996af9fa3
REAL_ADAMWEB <realadamweb@gmail.com>
2024-01-22 22:19:07 +0100

    Another Commit

--- app/src/main/java/com/adamweb/sarcoapp/AlbumViewHolder.java
+++ app/src/main/java/com/adamweb/sarcoapp/AlbumViewHolder.java
@@ -10,10 +10,7 @@ import com.makeramen.roundedimageview.RoundedImageView;
 
 public class AlbumViewHolder extends RecyclerView.ViewHolder {
 
-    RoundedImageView userAlbumCover;
-    TextView albumName, email, combination, phoneNumber;
-    public AlbumViewHolder(@NonNull View itemView) {
-        super(itemView);
+
 
         userAlbumCover = itemView.findViewById(R.id.myImage);
         email = itemView.findViewById(R.id.email);
