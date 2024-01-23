a29be84c6940f3c350bdc69dd7e121bad1718cc0
REAL_ADAMWEB <realadamweb@gmail.com>
2024-01-23 19:40:01 +0100

    Another Commit

--- app/src/main/java/com/adamweb/sarcoapp/CompleteProfile.java
+++ app/src/main/java/com/adamweb/sarcoapp/CompleteProfile.java
@@ -1,132 +1,15 @@
 package com.adamweb.sarcoapp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.webkit.MimeTypeMap;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-import com.github.dhaval2404.imagepicker.ImagePicker;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
-import de.hdodenhof.circleimageview.CircleImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CompleteProfile extends AppCompatActivity {
 
-    CircleImageView imageView;
-    FloatingActionButton floatingActionButton;
-    MaterialButton completeBtn;
-    ProgressBar progressBar;
-    FirebaseAuth firebaseAuth;
-    FirebaseUser firebaseUser;
-    StorageReference storageReference;
-    Uri profileImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_profile);
 
-        imageView = findViewById(R.id.circleImageView);
-        floatingActionButton = findViewById(R.id.floatingActionButton);
-        completeBtn = findViewById(R.id.completeBtn);
-        progressBar = findViewById(R.id.userProfileProgressBar);
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseUser = firebaseAuth.getCurrentUser();
-        storageReference = FirebaseStorage.getInstance().getReference("Users Pics");
-
-
-        completeBtn.setOnClickListener(v -> {
-            progressBar.setVisibility(View.VISIBLE);
-            userProfilePic();
-
-
-        });
-
-        floatingActionButton.setOnClickListener(v ->
-                ImagePicker.with(CompleteProfile.this)
-                .crop()	    			//Crop image(Optional), Check Customization for more option
-                .compress(1024)			//Final image size will be less than 1 MB(Optional)
-                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
-                .start());
-
-    }
-
-    private void userProfilePic() {
-        if (profileImage != null){
-            StorageReference imageFile = storageReference.child(firebaseAuth.getCurrentUser().getUid() + "." + getFileExtension(profileImage));
-            imageFile.putFile(profileImage).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    imageFile.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(Uri uri) {
-                            firebaseUser = firebaseAuth.getCurrentUser();
-                            UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder().setPhotoUri(uri).build();
-                            firebaseUser.updateProfile(userProfileChangeRequest);
-                        }
-                    });
-
-                    progressBar.setVisibility(View.GONE);
-                    Toast.makeText(CompleteProfile.this, "Wow You have Completed Your Profile", Toast.LENGTH_SHORT).show();
-                    homeActivity();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(CompleteProfile.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        } else {
-            progressBar.setVisibility(View.GONE);
-            Toast.makeText(this, "Please select your photo first!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private String getFileExtension(Uri profileImage) {
-        ContentResolver contentResolver = getContentResolver();
-        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
-        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(profileImage));
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        assert data != null;
-        profileImage = data.getData();
-        imageView.setImageURI(profileImage);
-    }
-
-
-    private void homeActivity (){
-        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        finish();
     }
 }
