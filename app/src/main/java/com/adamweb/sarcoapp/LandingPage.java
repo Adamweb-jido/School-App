c3c33d052404f4594e778838d28233a114c2d7e0
REAL_ADAMWEB <realadamweb@gmail.com>
2024-01-28 13:31:45 +0100

    Another Commit

--- app/src/main/java/com/adamweb/sarcoapp/LandingPage.java
+++ app/src/main/java/com/adamweb/sarcoapp/LandingPage.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LandingPage extends AppCompatActivity {
 
+    MaterialButton letsGo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        letsGo = findViewById(R.id.goBtn);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
     }
 }
