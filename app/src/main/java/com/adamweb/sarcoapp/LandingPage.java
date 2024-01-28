c4ed01080a57fec4fb9a10b34a1f2378538f47f4
REAL_ADAMWEB <realadamweb@gmail.com>
2024-01-28 12:09:05 +0100

    Another Commit

--- app/src/main/java/com/adamweb/sarcoapp/LandingPage.java
+++ app/src/main/java/com/adamweb/sarcoapp/LandingPage.java
@@ -3,6 +3,7 @@ package com.adamweb.sarcoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
