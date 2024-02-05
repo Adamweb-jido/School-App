00909c65d3288793bd7c8f7b0930ae960f41f5a9
REAL_ADAMWEB <realadamweb@gmail.com>
2024-02-05 22:44:50 +0100

    Another Commit

--- app/src/main/java/com/adamweb/sarcoapp/EditProfile.java
+++ app/src/main/java/com/adamweb/sarcoapp/EditProfile.java
@@ -3,8 +3,22 @@ package com.adamweb.sarcoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class EditProfile extends AppCompatActivity {
+
+    TextView slideOne, slideTwo;
+    EditText editFirstName, editLastName, editPhoneNumber, editEmailAddress, editComment;
+    MaterialButton saveChangesBtn, cancelChangesBtn;
+    ImageView backArrow, profilePic;
+    FloatingActionButton floatingActionButton;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
