f015013fc850a3f9fe2fa7f589312a112b1de046
REAL_ADAMWEB <realadamweb@gmail.com>
2024-02-05 22:53:57 +0100

    Another Commit

--- app/src/main/java/com/adamweb/sarcoapp/EditProfile.java
+++ app/src/main/java/com/adamweb/sarcoapp/EditProfile.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
@@ -18,11 +19,27 @@ public class EditProfile extends AppCompatActivity {
     MaterialButton saveChangesBtn, cancelChangesBtn;
     ImageView backArrow, profilePic;
     FloatingActionButton floatingActionButton;
+    ProgressBar progressBar;
     FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        slideOne = findViewById(R.id.textView4);
+        slideTwo = findViewById(R.id.slideTwo);
+        editFirstName = findViewById(R.id.editFirstName);
+        editLastName = findViewById(R.id.editLastName);
+        editPhoneNumber = findViewById(R.id.editPhoneNumber);
+        editEmailAddress = findViewById(R.id.editEmailAddress);
+        editComment = findViewById(R.id.editComment);
+        saveChangesBtn = findViewById(R.id.saveChangesBtn);
+        cancelChangesBtn = findViewById(R.id.cancelBtn);
+        backArrow = findViewById(R.id.backArrow);
+        profilePic = findViewById(R.id.profile_image);
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        progressBar = findViewById(R.id.editProgressBar);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 }
