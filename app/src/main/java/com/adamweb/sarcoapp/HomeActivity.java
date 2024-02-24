38388b52c3cddd332a362c7dcc311a20394459c9
REAL_ADAMWEB <realadamweb@gmail.com>
2024-02-24 12:16:39 +0100

    Another Commit

--- app/src/main/java/com/adamweb/sarcoapp/HomeActivity.java
+++ app/src/main/java/com/adamweb/sarcoapp/HomeActivity.java
@@ -13,6 +13,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
@@ -124,6 +125,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         setImageSlider();
         setLeadersItems();
         displayAllUsers();
+
+        navCancelBtn.setOnClickListener(v ->{
+            drawerLayout.closeDrawer(GravityCompat.END);
+        });
 
         home.setOnClickListener(v ->{
             progressBar.setVisibility(View.VISIBLE);
